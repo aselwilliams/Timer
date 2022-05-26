ea5186a1d2b3c9629fd8ceed4fc7433ba898ad41
aselwilliams <aselwilliams22@gmail.com>
2022-05-26 14:14:01 -0400

    added sound

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Timer from "./Timer";
 
 function App() {
+  const [targetTime, setTargetTime] = useState(0);
+
+  const handleTimeChange = (e) => {
+    e.preventDefault();
+    setTargetTime(e.target.value * 1000);
+  };
+  console.log(targetTime);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form className="card-box">
+        <label htmlFor="start-time">Set Timer value in seconds: </label>
+        <input
+          placeholder="00s"
+          type="number"
+          id="start-time"
+          pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}"
+          onChange={handleTimeChange}
+        />
+      </form>
+      <Timer targetTime={targetTime} />
+    </section>
   );
 }
-
 export default App;
+
---
+++ src/Timer.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+
+const Timer = ({ targetTime }) => {
+  const [time, setTime] = useState(60000);
+  const [timerOn, setTimerOn] = useState(true);
+  const [intervalID, setIntervalID] = useState(0);
+
+  useEffect(() => {
+    let interval = null;
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((prevState) => prevState - 1000);
+        // setIntervalID(interval);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    if (time == 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [time, timerOn, targetTime]);
+
+  const handleReset = () => {
+    setTime(0);
+    setTimerOn(false);
+  };
+
+  const handleSetSeconds = (targetTime) => {
+    setTime(targetTime);
+  };
+
+  const playBeep = () => {
+    const audio = new Audio(
+      "https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3"
+    );
+    audio.play();
+  };
+
+  if (time<= 0) {
+    playBeep();
+  }
+  return (
+    <div>
+      <main className="card-box">
+        <h1>Countdown</h1>
+        <div id="display-timer">
+          <span>{("0" + Math.floor((time / 3600000) % 60)).slice(-2)} :</span>
+          <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)} : </span>
+          <span>{("0" + ((time / 1000) % 60)).slice(-2)}</span>
+        </div>
+        <div>
+          <button onClick={()=>handleSetSeconds(targetTime)} className="set">Start</button>
+          <button
+            onClick={() => setTimerOn(!timerOn)}
+            className={timerOn ? "stop" : "resume"}
+          >
+            {timerOn ? "Pause Timer" : "Resume Timer"}
+          </button>
+          <button onClick={handleReset} className="reset">Reset</button>
+        </div>
+      </main>
+    </div>
+  );
+};
+export default Timer;
